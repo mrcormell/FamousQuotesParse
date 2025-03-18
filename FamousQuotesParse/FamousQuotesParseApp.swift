7c3263365deb92c9dd2ca05a50a3ca8d116ad5cb
David Cormell <d.cormell@etoncollege.org.uk>
2025-03-18 16:20:47 +0000

    Initial Commit

---
+++ FamousQuotesParse/FamousQuotesParseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamousQuotesParseApp.swift
+//  FamousQuotesParse
+//
+//  Created by Cormell, David - DPC on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FamousQuotesParseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FamousQuotesParseTests/FamousQuotesParseTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamousQuotesParseTests.swift
+//  FamousQuotesParseTests
+//
+//  Created by Cormell, David - DPC on 18/03/2025.
+//
+
+import Testing
+@testable import FamousQuotesParse
+
+struct FamousQuotesParseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
